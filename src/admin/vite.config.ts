454ab865d80cf67aaed0976023d3af476460cc3b
psp251 <90175108+psp251@users.noreply.github.com>
2024-10-09 18:19:07 +0530

    Update vite.config.ts

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -3,13 +3,13 @@ import { mergeConfig, type UserConfig } from 'vite';
 export default (config: UserConfig) => {
     // Important: always return the modified config
     return mergeConfig(config, {
+        server: {
+            port: 5174, // Replace with your desired port number
+        },
         resolve: {
             alias: {
                 '@': '/src',
             },
-            server: {
-                port: 5174, // Replace with your desired port number
-            }
         },
     });
 };
